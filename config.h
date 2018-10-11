42fd7ad68af50272b7149c8d1ebea5290b678ebb
mmcdole <simucal@gmail.com>
2018-10-11 12:51:13 -0500

    Update config.h

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xFC;
+unsigned int alpha = 0xDC;
 
 static const char *colorname[] = {
 	"#073642",  /*  0: black    */
